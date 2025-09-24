5da48031176476a8eac0b9e1d4df659f698efd0a
jean-emmanuel <jean-emmanuel.doucet@groolot.net>
2025-09-24 11:46:32 +0200

    ffclock: aes default to xlr

--- fireface_control/fireface.py
+++ fireface_control/fireface.py
@@ -554,7 +554,7 @@ class FireFace(Module):
                           alsa='iface=CARD,name=\'primary-clock-source\'', osc=True)
         self.add_parameter('optical-output-signal', None, types='i', default=0,
                           alsa='iface=CARD,name=\'optical-output-signal\'', osc=True)
-        self.add_parameter('spdif-input-interface', None, types='i', default=1,
+        self.add_parameter('spdif-input-interface', None, types='i', default=0,
                           alsa='iface=CARD,name=\'spdif-input-interface\'', osc=True)
         self.add_parameter('spdif-output-format', None, types='i', default=1,
                           alsa='iface=CARD,name=\'spdif-output-format\'', osc=True)
